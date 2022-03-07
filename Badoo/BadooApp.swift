c098b9df8990b08f6a42d299d1b9a6ced5c1c43f
Mohamed Alwakil <mohamed.diaa@qvik.fi>
2022-03-07 17:51:43 +0100

    Initial Commit

---
+++ Badoo/BadooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadooApp.swift
+//  Badoo
+//
+//  Created by Mohamed Alwakil on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct BadooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
